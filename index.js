6b0458e04b25f83b10cd97efe26c5e3f9734b859
Ian <iankosgey535@gmail.com>
2024-01-08 11:50:32 +0300

    Practiced ContentLoaded

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+const id = document.getElementById('text');
+document.addEventListener('DOMContentLoaded', function(){
+    if(id){
+        id.textContent = 'This is really cool!';
+    }
+});
